9512f8c0ad344b4269955e7f74102c68f0e520f0
Pratikshya0809 <pratikshyakunwar0809@gmail.com>
2025-06-14 15:35:55 +0545

    direction used by Pacman to manually add a turn

--- src/Entities/Ghost.java
+++ src/Entities/Ghost.java
@@ -70,4 +70,100 @@ public class Ghost extends MovingEntity {
      * Unqueues a turn from the queue and tries to perform it on the Node.
      * @param n The Node to perform the turn on.
      */
+ @Override
+    public void makeTurn(Node n) {
+        // If there is priority queue unqueue that one,
+        // otherwise super.makeTurn()
+        if (priorityQueue.isEmpty()) {
+            super.makeTurn(n);
+        }else if (n.canTurn(priorityQueue.getFirst())) {
+            setDirection(priorityQueue.getFirst());
+            setCurrEdge(n.getTurn(priorityQueue.removeFirst()));
+        }else {
+            priorityQueue.removeFirst();
+        }
+        
+    }
+    
+    /**
+     * Handles the decision-making when it comes to choosing which turn to perform next.
+     * @param direction used by Pacman to manually add a turn
+     */
+    @Override
+    public void addTurn(EDirection direction) {
+        // TODO: Implement if close to pacman
+        //          1. If not vulnerable
+        //                  tries to get closer to Pacman
+        //          2. If vulnerable
+        //                  tries to get away from Pacman
+        //      otherwise a random one
+        
+        if (getCurrEdge().isAtExtremes(this)) {
+            // The possible turns to be performed on the Node
+            ArrayList<EDirection> possible_turns = getCurrEdge().getExtreme(this).getPossibleTurns();
+            
+            // The inverse of the current Ghost direction
+            EDirection inverse = null;
+            if(getDirection()!=null) {
+                switch(getDirection()) {
+                    case UP:
+                        inverse = EDirection.DOWN;
+                        break;
+                    case DOWN:
+                        inverse = EDirection.UP;
+                        break;
+                    case LEFT:
+                        inverse = EDirection.RIGHT;
+                        break;
+                    default: // RIGHT
+                        inverse = EDirection.LEFT;
+                        break;
+                }
+            }
+            
+            // Removes the inverse of the direction from the possible turns,
+            //  to eliminate the strange behaviour of going back at a turn.
+            if (possible_turns.size()>1)
+                possible_turns.remove(inverse);
+            
+            // Chooses a random turn to add to the queue
+            Random r = new Random();
+            turnQueue.add(possible_turns.get(new Random().nextInt(possible_turns.size())));
+        }
+    }
+
+    public void setVulnerable(boolean to) {
+        vulnerable = to;
+        if (vulnerable) {
+            setImage(EImage.ghost_vuln);
+            setSpeed(((int)Settings.get(EParam.ghost_vuln_speed)));
+        } else {
+            resetGhostImg();
+            setSpeed((int)Settings.get(EParam.ghost_speed));
+        }
+    }
+
+    /**
+     * Sets the Ghost image accordingly to the type of the ghost
+     */
+    public void resetGhostImg() {
+        EImage img;
+        switch(type) {
+            case ghost1:
+                img = EImage.ghost1_left;
+                break;
+            case ghost2:
+                img = EImage.ghost2_right;
+                break;
+            case ghost3:
+                img = EImage.ghost3_right;
+                break;
+            default:
+                img = EImage.ghost4_right;
+                break;
+        }
+        setImage(img);
+    }
+    
+    ////////////////
 }
